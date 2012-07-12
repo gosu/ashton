b8f4ce46276ec8b20ae6774a5afe9d08ebfcecb0
Bil Bas (Spooner) <bil.bagpuss@gmail.com>
2012-07-12 20:04:00 +0100

    Added another particle type to example.

--- examples/particles_example.rb
+++ examples/particles_example.rb
@@ -19,33 +19,43 @@ class TestWindow < Gosu::Window
     @star = Gosu::Image.new self, media_path("SmallStar.png"), true
     @background = Gosu::Image.new self, media_path("Earth.png"), true
 
-    @image_emitter = Ashton::ParticleEmitter.new 100, 300, 0,
-                                            image: @star,
-                                            scale: 0.3,
-                                            speed: 1, time_to_live: 5,
-                                            acceleration: -2,
-                                            max_particles: 2000,
-                                            interval: 0.01
+    @image_emitter = Ashton::ParticleEmitter.new 450, 100, 0,
+                                                 image: @star,
+                                                 scale: 0.3,
+                                                 speed: 1, time_to_live: 5,
+                                                 acceleration: -2,
+                                                 max_particles: 2000,
+                                                 interval: 0.01
 
-    @shaded_image_emitter = Ashton::ParticleEmitter.new 400, 200, 0,
-                                            image: @star, friction: 1,
-                                            shader: @grayscale,
-                                            speed: 2, time_to_live: 5,
-                                            interval: 0.02, position_offset: 24
+    @shaded_image_emitter = Ashton::ParticleEmitter.new 450, 350, 0,
+                                                        image: @star, friction: 1,
+                                                        shader: @grayscale,
+                                                        speed: 2, time_to_live: 5,
+                                                        interval: 0.02, position_offset: 24
 
-    @point_emitter = Ashton::ParticleEmitter.new 100, 50, 0,
-                                            scale: 12, color: Gosu::Color::RED,
-                                            speed: 2, time_to_live: 4,
-                                            interval: 0.001,
-                                            max_particles: 3000,
-                                            interval: 0.003,
-                                            color: Gosu::Color.rgba(255, 0, 0, 50)
+    @point_emitter = Ashton::ParticleEmitter.new 100, 100, 0,
+                                                 scale: 12,
+                                                 speed: 2, time_to_live: 4,
+                                                 interval: 0.001,
+                                                 max_particles: 3000,
+                                                 interval: 0.003,
+                                                 color: Gosu::Color.rgba(255, 0, 0, 50)
+
+    @shaded_point_emitter = Ashton::ParticleEmitter.new 100, 350, 0,
+                                                        scale: 12, shader: @grayscale,
+                                                        speed: 2, time_to_live: 4,
+                                                        speed_deviation: 0.5,
+                                                        interval: 0.001,
+                                                        max_particles: 3000,
+                                                        interval: 0.003,
+                                                        color: Gosu::Color.rgba(255, 0, 0, 255)
   end
 
   def update
     @image_emitter.update        unless button_down? Gosu::Kb1
     @shaded_image_emitter.update unless button_down? Gosu::Kb2
     @point_emitter.update        unless button_down? Gosu::Kb3
+    @shaded_point_emitter.update unless button_down? Gosu::Kb4
   end
 
   def button_down(id)
@@ -57,11 +67,12 @@ class TestWindow < Gosu::Window
   def draw
     @background.draw 0, 0, 0, width.fdiv(@background.width), height.fdiv(@background.height), Gosu::Color.rgba(255, 255, 255, 175)
 
+    @shaded_point_emitter.draw
+    @point_emitter.draw
     @image_emitter.draw
     @shaded_image_emitter.draw
-    @point_emitter.draw
 
-    @font.draw "FPS: #{Gosu::fps} Im: #{@image_emitter.size} ImSha: #{@shaded_image_emitter.size} Pt: #{@point_emitter.size}", 0, 0, 0
+    @font.draw "FPS: #{Gosu::fps} Img: #{@image_emitter.size} ShaImg: #{@shaded_image_emitter.size} Pnt: #{@point_emitter.size} ShaPnt: #{@shaded_point_emitter.size}", 0, 0, 0
   end
 end
 
