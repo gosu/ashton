36a018ef0913f3a8b9be3890d318f3273932717c
Bil Bas (Spooner) <bil.bagpuss@gmail.com>
2012-08-04 14:43:06 +0100

    Added another emitter tied to the mouse cursor.

--- examples/particle_emitter_example.rb
+++ examples/particle_emitter_example.rb
@@ -11,7 +11,7 @@ def media_path(file); File.expand_path "media/#{file}", File.dirname(__FILE__) e
 class TestWindow < Gosu::Window
   def initialize
     super 640, 480, false
-    self.caption = "Particle emitters"
+    self.caption = "Particle emitters - 4 emitters at static positions and one on mouse"
 
     @grayscale = Ashton::Shader.new fragment: :grayscale
 
@@ -31,7 +31,7 @@ class TestWindow < Gosu::Window
     @shaded_image_emitter = Ashton::ParticleEmitter.new 450, 350, 0,
                                                         image: @star,
                                                         shader: @grayscale,
-                                                        interval: 0.0002,
+                                                        interval: 0.00025,
                                                         offset: 0..10,
                                                         max_particles: 15000,
                                                         angular_velocity: 20..50,
@@ -55,9 +55,20 @@ class TestWindow < Gosu::Window
                                                         time_to_live: 12,
                                                         interval: 0.0003,
                                                         max_particles: 15000,
-                                                        color: Gosu::Color.rgba(255, 0, 0, 255),
+                                                        color: Gosu::Color.rgba(255, 0, 0, 100),
                                                         gravity: 60 # pixels/s*s
+
+    @mouse_emitter = Ashton::ParticleEmitter.new 0, 0, 3,
+                                                 scale: 4,
+                                                 speed: 20..50,
+                                                 offset: 0..5,
+                                                 interval: 0.0025,
+                                                 color: Gosu::Color.rgba(0, 255, 255, 100),
+                                                 fade: 50,
+                                                 gravity: 60 # pixels/s*s
   end
+
+  def needs_cursor?; true end
 
   def update
     $gosu_blocks.clear # workaround for Gosu 0.7.45 bug.
@@ -71,6 +82,9 @@ class TestWindow < Gosu::Window
     @shaded_image_emitter.update delta unless button_down? Gosu::Kb2
     @point_emitter.update delta unless button_down? Gosu::Kb3
     @shaded_point_emitter.update delta unless button_down? Gosu::Kb4
+
+    @mouse_emitter.x, @mouse_emitter.y = mouse_x, mouse_y
+    @mouse_emitter.update delta unless button_down? Gosu::Kb5
   end
 
   def button_down(id)
@@ -84,11 +98,15 @@ class TestWindow < Gosu::Window
     @shaded_image_emitter.draw
     @shaded_point_emitter.draw
     @image_emitter.draw
+    @mouse_emitter.draw
 
-    num_particles = @point_emitter.count + @shaded_point_emitter.count + @image_emitter.count + @shaded_image_emitter.count
+    num_particles = @point_emitter.count + @shaded_point_emitter.count +
+                    @image_emitter.count + @shaded_image_emitter.count +
+                    @mouse_emitter.count
     @font.draw "FPS: #{Gosu::fps}   Particles: #{num_particles}", 0, 0, Float::INFINITY
 
-    @font.draw_rel "Pnt: #{@point_emitter.count} ShaPnt: #{@shaded_point_emitter.count} Img: #{@image_emitter.count} ShaImg: #{@shaded_image_emitter.count}", 0, height, Float::INFINITY, 0, 1
+    totals = "Pnt: #{@point_emitter.count} ShaPnt: #{@shaded_point_emitter.count} Img: #{@image_emitter.count} ShaImg: #{@shaded_image_emitter.count} Mouse: #{@mouse_emitter.count}"
+    @font.draw_rel totals, 0, height, Float::INFINITY, 0, 1
   end
 end
 
